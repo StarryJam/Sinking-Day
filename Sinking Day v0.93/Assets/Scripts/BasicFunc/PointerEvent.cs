eab16bbee17cd3df55f1edfca3ff3d17fee78dcf
StaryJam <40786345+StaryJam@users.noreply.github.com>
2019-05-16 22:47:06 +0800

    v0.931
    进一步优化了鼠标点击事件

---
+++ Sinking Day v0.93/Assets/Scripts/BasicFunc/PointerEvent.cs
@@ -0,0 +1,163 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using cakeslice;
+
+public class PointerEvent : MonoBehaviour {
+
+    public static GameObject selected;
+    //public List<string> selectableObj;
+    public static bool isCasting = false;
+    public static Skill castingSkill;
+    public static RaycastHit hit;
+    public static PointerState _pointerState;
+
+    public static bool isOnMap = false;
+    public static bool isOnUI = false;
+    public static bool isOnUnit = false;
+    public static bool canSelecte = false;
+    public static bool isOnEnemy = false;
+    public static bool isOnFriend = false;
+
+    public static GameObject pointerOnObj;
+
+    public enum PointerState
+    {
+        normal,
+        choosingTarget
+    }
+
+    public static void ChangingPointerState(PointerState state)
+    {
+        _pointerState = state;
+        if (_pointerState == PointerState.normal)
+        {
+            Cursor.SetCursor(UIManager.defaultPointer, Vector2.zero, CursorMode.Auto);
+        }
+        if (_pointerState == PointerState.choosingTarget)
+        {
+            Cursor.SetCursor(UIManager.onPointPointer, new Vector2(100, 100), CursorMode.Auto);
+        }
+    }
+
+    private void PointerOnWhat()
+    {
+        
+        if (EventSystem.current.IsPointerOverGameObject() == true)//判断鼠标是否在UI上
+            isOnUI = true;
+        else
+            isOnUI = false;
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        bool isCollider = Physics.Raycast(ray, out hit, 1000);
+        if (isCollider)
+        {
+            pointerOnObj = hit.collider.gameObject;
+
+            //判断鼠标是否在地图上
+            if (pointerOnObj.GetComponent<Map_BaseCube>() != null)
+                isOnMap = true;
+            else
+                isOnMap = false;
+
+            //判断是否在单位上
+            if (pointerOnObj.GetComponent<Unit>() != null)
+            {
+                isOnUnit = true;
+                if (pointerOnObj.GetComponent<Unit>().camp == Unit.Camp.enemy)
+                {
+                    isOnEnemy = true;
+                }
+            }
+            else
+                isOnUnit = false;
+
+            //判断能否选择
+            if (pointerOnObj.GetComponent<Selectee>() != null)
+                canSelecte = true;
+            else
+                canSelecte = false;
+        }
+        else
+        {
+            pointerOnObj = null;
+        }
+    }
+
+    public void Select()
+    {
+        if (pointerOnObj != null)
+        {
+            if(pointerOnObj.GetComponent<Selectee>() != null)
+            {
+                if (selected == null)
+                {
+                    pointerOnObj.GetComponent<Selectee>().BeSelected();
+                }
+                else
+                {
+                    //点击不相同目标的时候重新选择
+                    if (selected != pointerOnObj)
+                    {
+                        selected.GetComponent<Selectee>().DeSelected();
+                        Select();
+                    }
+                }
+            }
+        }
+    }
+    
+
+    public void Cancel()
+    {
+        if (selected.GetComponent<UnitOfPlyer>() != null)
+        {
+            selected.GetComponent<UnitOfPlyer>().Cancel();
+        }
+        else
+            selected.GetComponent<Selectee>().DeSelected();
+        //StartCoroutine(DeSelectAtTheEndOfFrame());
+    }
+
+    private IEnumerator DeSelectAtTheEndOfFrame()
+    {
+        yield return new WaitForEndOfFrame();
+        selected.GetComponent<Selectee>().DeSelected();
+        selected = null;
+    }
+
+
+    
+    void Update () {
+        PointerOnWhat();
+
+        if (Input.GetMouseButtonUp(0))//鼠标左键松开
+        {
+            if (!isOnUI)//判断是否点击在UI上
+            {
+                if (_pointerState == PointerState.choosingTarget)//判断是否正在释放技能
+                {
+                    //castingSkill.CastSkill();
+                    selected.GetComponent<UnitOfPlyer>().DoAction();
+                }
+                else
+                {
+                    Select();
+                }
+            }
+        }
+        else if (Input.GetMouseButtonUp(1))//鼠标右键松开
+        {
+            if (selected != null)
+            {
+                //若玩家正在操作单位则不取消选择
+                    Cancel();
+            }
+            if (isCasting)
+            {
+                castingSkill.StopCast();
+            }
+        }
+    }
+}
---
+++ Sinking Day v0.93/Assets/Scripts/Unit/Unit.cs
@@ -0,0 +1,203 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using cakeslice;
+
+public class Unit : MonoBehaviour,Selectee  {
+
+    public Grid map;
+    public float maxHealth = 100;
+    public float currentHealth;
+    public int attackRange = 5;
+    public float speed = 10;
+
+    public float attackDamage = 10;
+
+    
+
+    public static RaycastHit hit;
+    public Canvas stateHudUI;
+    public Canvas rangeCursorUI;
+    public Transform statementUIPos;
+    public Transform rangeCursorPos;
+
+    protected bool isMoving = false;
+    public UnitState state;
+    public Camp camp;
+
+
+
+    public void Start()
+    {
+        //UI初始化
+        statementUIPos = transform.Find("StatementUIPos");
+        rangeCursorPos = transform.Find("RangeCursorPos");
+        stateHudUI = UIManager.CreateUI(UIManager.UI_UnitStateHud, statementUIPos);
+        stateHudUI.GetComponent<UnitStateUI>().unit = this;
+        rangeCursorUI = UIManager.CreateUI(UIManager.UI_RangeCursorOnUnit, rangeCursorPos);
+        UIManager.HideUI(rangeCursorUI);
+        
+
+        map = Grid.map;
+        currentHealth = maxHealth;
+        state = UnitState.holding;
+    }
+
+    public enum Camp //阵营
+    {
+        friend,
+        enemy
+    }
+
+    public enum UnitState
+    {
+        holding,
+        readyToMove,
+        readyToAttack
+    }
+
+    public void ChangingState(UnitState _state)
+    {
+        state = _state;
+        if (_state == UnitState.holding)
+        {
+            PointerEvent.ChangingPointerState(PointerEvent.PointerState.normal);
+        }
+        if (_state == UnitState.readyToAttack)
+        {
+            PointerEvent.ChangingPointerState(PointerEvent.PointerState.choosingTarget);
+        }
+    }
+
+    public void BeSelected()
+    {
+        PointerEvent.selected = gameObject;
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.AddComponent<Outline>();
+        }
+    }
+
+    public void DeSelected()
+    {
+        PointerEvent.selected = null;
+        ChangingState(UnitState.holding);
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Destroy(transform.GetChild(i).gameObject.GetComponent<Outline>());
+        }
+    }
+
+
+
+    public void LateUpdate () {
+        
+    }
+
+    //public void ChoosingPath()
+    //{
+    //        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    //        bool isCollider = Physics.Raycast(ray, out hit, 1000);
+
+    //        if (isCollider)
+    //        {
+    //            if (hit.collider.gameObject.GetComponent<Map_BaseCube>() != null) //判断是否在地图上
+    //            {
+    //                map.FindPath(transform.position, hit.collider.transform.position);
+                    
+    //            }
+    //        }
+    //}
+
+    public void ChoosePath(Map_BaseCube destination)
+    {
+        map.FindPath(transform.position, hit.collider.transform.position);
+    }
+
+
+
+    public void Move()
+    {
+        StartCoroutine(MovePath(map.path));
+        state = UnitState.holding;
+        map.path.Clear();
+    }
+
+    IEnumerator MovePath(List<Node> _path)
+    {
+        List<Node> path = new List<Node>();
+        for(int i = 0; i < _path.Count; i++)
+        {
+            path.Add(_path[i]);
+        }
+        isMoving = true;
+        if (path.Count > 0)
+        {
+            for (int i = 0; i < path.Count; i++)
+            {
+                yield return StartCoroutine(MoveAStep(path[i]._worldPos));
+            }
+        }
+        isMoving = false;
+    }
+
+    IEnumerator MoveAStep(Vector3 toPositon)
+    {
+        isMoving = true;
+        float schedule = 0;//动画插值
+        Vector3 from = transform.position;
+        Vector3 to = toPositon;
+        float time = Vector3.Distance(from, to) / speed;
+        float scheduleSpeed = 1 / time;
+        while (schedule < 1)
+        {
+            schedule += Time.deltaTime * scheduleSpeed;
+            if (schedule > 1)
+                schedule = 1;
+
+            transform.position = Vector3.Lerp(from, to, schedule);
+            yield return new WaitForEndOfFrame();
+        }
+        isMoving = false;
+    }
+
+
+    public void ChoosingAttackTarget()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        bool isCollider = Physics.Raycast(ray, out hit, 1000);
+
+        if (isCollider)
+        {
+
+        }
+    }
+
+    public void Attack(Unit unit)
+    {
+        unit.takeDamage(attackDamage);
+    }
+
+    public void takeDamage(float damage)
+    {
+        currentHealth -= damage;
+        UpdateState();
+    }
+
+    private void UpdateState()
+    {
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+
+}
