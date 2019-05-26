323c670a8748f1fdf2002b6343a707a74f6c3a75
StaryJam <40786345+StaryJam@users.noreply.github.com>
2019-05-26 21:29:11 +0800

    Revert "Revert "v0.971""
    This reverts commit bd13e76f79ca69202901ad68b2f2f44f53c439f0.

---
+++ Sinking Day/Assets/LoadSceneBtn.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadSceneBtn : MonoBehaviour {
+
+    public string nextSceneName;
+    private Button btn;
+
+    private void Start()
+    {
+        btn = GetComponent<Button>();
+        btn.onClick.AddListener(LoadScene);
+    }
+
+    private void LoadScene()
+    {
+        LevelManager0.LoadNextScene(nextSceneName);
+    }
+
+}
---
+++ Sinking Day/Assets/Scripts/GameManagers/LevelManager0.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager0 : MonoBehaviour {
+
+    public static string nextSceneName;
+    public static LevelManager0 levelManager = null;
+
+    private void Awake()
+    {
+        if (levelManager == null)
+        {
+            levelManager = this;
+            nextSceneName = "MainMenu";
+        }
+        else
+            Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public static void LoadNextScene(string name)
+    {
+        nextSceneName = name;
+        SceneManager.LoadScene("Loading");
+    }
+
+    
+}
